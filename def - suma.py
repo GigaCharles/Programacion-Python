d695324c5508f1d146097b56fb27ef617167c8da
GigaCharles <145706526+GigaCharles@users.noreply.github.com>
2023-12-13 10:14:44 -0500

    Add files via upload

---
+++ def - suma.py
@@ -0,0 +1,11 @@
+
+def suma (num1,num2):
+    respuesta = num1 + num2
+    return respuesta 
+
+numero1 = float(input("Ingrese un numero: "))
+numero2 = float(input("Ingrese otro numero: "))
+
+resolucion = suma(numero1, numero2)
+
+print("La suma de los numeros ingresados es: ", resolucion)
